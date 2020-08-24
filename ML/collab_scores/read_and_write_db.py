b09b9bb1b639c301fbe5de1a913381b48c9b9e28
Poorvi <41854709+poorvirhebbar@users.noreply.github.com>
2020-08-24 17:05:13 +0530

    Add files via upload

--- ML/collab_scores/read_and_write_db.py
+++ ML/collab_scores/read_and_write_db.py
@@ -1,70 +1,97 @@
-import mysql.connector
-import datetime
 import pandas as pd
+import sqlalchemy
+import datetime
 from algorithm import modified_hot_ranking_computation
 from collab import get_collab_score
 
+connection_params = dict(entry.split('=') for entry in open('connection_details.txt').read().split(','))
+
+db_user = connection_params['username']
+db_pass = connection_params['password']
+db_name = connection_params['database']
+db_socket = connection_params['unix_socket']
+db_host = connection_params['host']
+
 def single_collab_score(df, user_id, meme_id, score_list):
+
 	df1=df[df['user_id'] == user_id]
 	idx=df1[df1['meme_id'] == meme_id].index[0]
+	
 	return score_list[idx]
-
 
 def read_and_write_db():
 
-	connection_params=dict(entry.split('=') for entry in open('connection_details').read().split(','))
-	connection_params['database']=db_name
+	print("begin rw_db")
+	
+	db = sqlalchemy.create_engine(
+			sqlalchemy.engine.url.URL(
+					drivername="mysql+pymysql",
+					username=db_user,  
+					password=db_pass, 
+					database=db_name,  
+					host=db_host
+			)
+	)
 
-	connection=mysql.connector.connect(**connection_params)
-	cursor = connection.cursor()
+	with db.connect() as conn:
 
+		conn.execute("DROP TABLE IF EXISTS `dislikes`")
+		conn.execute("DROP TABLE IF EXISTS `likes`")
+		conn.execute("DROP TABLE IF EXISTS `rofls`")
+		conn.execute("DROP VIEW IF EXISTS `dislikes`")
+		conn.execute("DROP VIEW IF EXISTS `likes`")
+		conn.execute("DROP VIEW IF EXISTS `rofls`")
+		conn.execute("CREATE VIEW `dislikes` AS SELECT meme_id, count(*) AS c FROM user_meme_interaction WHERE reaction=3 GROUP BY meme_id;")
+		conn.execute("CREATE VIEW `likes` AS SELECT meme_id, count(*) AS c FROM user_meme_interaction WHERE reaction=2 GROUP BY meme_id;")
+		conn.execute("CREATE VIEW `rofls` AS SELECT meme_id, count(*) AS c FROM user_meme_interaction WHERE reaction=1 GROUP BY meme_id;")
 
-	cursor.execute("CREATE VIEW `dislikes` AS SELECT meme_id, count(*) AS c FROM user_meme_interaction WHERE reaction=`dislike` GROUP BY meme_id;")
-	cursor.execute("CREATE VIEW `likes` AS SELECT meme_id, count(*) AS c FROM user_meme_interaction WHERE reaction=`like` GROUP BY meme_id;")
-	cursor.execute("CREATE VIEW `rofls` AS SELECT meme_id, count(*) AS c FROM user_meme_interaction WHERE reaction=`rofl` GROUP BY meme_id;")#tobecorrected
+		rows = conn.execute("SELECT user_id, meme_id FROM user_meme_interaction;").fetchall()
 
-	cursor.execute("SELECT user_id, meme_id FROM user_meme_interaction;")
+		df = pd.DataFrame(rows)
+		score_list = get_collab_score(df)
 
-	rows= cursor.fetchall()
-	df = pd.DataFrame(rows)
+		for row in rows:
+			user_id=row[0]
+			user_topics_rows = conn.execute("SELECT topic FROM user_topics WHERE user_id=%s;",(user_id)).fetchall()
+			user_topics=[]
+			for urow in user_topics_rows:
+				user_topics.append(urow[0])
 
-	score_list = get_collab_score(df)
+			meme_id=row[1]
+			meme_topics_rows = conn.execute("SELECT topic FROM meme_topics WHERE meme_id=%s;",(meme_id)).fetchall()
+			meme_topics=[]
+			for mrow in meme_topics_rows:
+				meme_topics.append(mrow[0])
 
-	for row in rows:
+			reactions=[]
+			
+			cur_res = conn.execute("SELECT c FROM dislikes WHERE meme_id=%s;",(meme_id)).fetchone()
+			if (cur_res != None):
+				reactions.append(cur_res[0])
+			else:
+				reactions.append(0)
+			
+			cur_res = conn.execute("SELECT c FROM likes WHERE meme_id=%s;",(meme_id)).fetchone()
+			if (cur_res != None):
+				reactions.append(cur_res[0])
+			else:
+				reactions.append(0)
+			
+			
+			cur_res = conn.execute("SELECT c FROM rofls WHERE meme_id=%s;",(meme_id)).fetchone()
+			if (cur_res != None):
+				reactions.append(cur_res[0])
+			else:
+				reactions.append(0)
+			
 
-		rating_user_meme=
-		user_id=row[0]
-		cursor.execute("SELECT topic FROM user_topics WHERE user_id=%s;",(user_id))
-		user_topics_rows=cursor.fetchall()
-		user_topics=[]
-		for urow in user_topics_rows:
-			user_topics.append(urow[0])
+			timestamp=(conn.execute("SELECT upload_time FROM memes WHERE id=%s;",(meme_id)).fetchone())[0]
 
-		meme_id=row[1]
-		cursor.execute("SELECT topic FROM user_topics WHERE user_id=%s;",(meme_id))
-		meme_topics_rows=cursor.fetchall()
-		meme_topics=[]
-		for mrow in meme_topics_rows:
-			meme_topics.append(mrow[0])#tobecorrected
+			score1=modified_hot_ranking_computation(reactions, timestamp, user_topics, meme_topics)
 
-		reactions=[]
-		cursor.execute("SELECT c FROM dislikes WHERE meme_id=%s;",(meme_id))
-		reactions.append(cursor.fetchone()[0])
-		cursor.execute("SELECT c FROM likes WHERE meme_id=%s;",(meme_id))
-		reactions.append(cursor.fetchone()[0])
-		cursor.execute("SELECT c FROM rofls WHERE meme_id=%s;",(meme_id))
-		reactions.append(cursor.fetchone()[0])
+			score2=single_collab_score(df, user_id, meme_id, score_list)
 
-		timestamp=datetime.datetime().now()
+			weights=[1,1]
+			score=score1*weights[0]+score2*weights[1]
 
-		score1=str(modified_hot_ranking_computation(reactions, timestamp, user_topics, meme_topics))
-		score2=single_collab_score(df, user_id, meme_id, score_list)
-
-		weights=[1,1]
-		score=score1*weights[0]+score2*weights[1]
-
-		cursor.execute("UPDATE user_meme_interaction SET score=%s WHERE user_id=%s, meme_id=%s;",(score,user_id,meme_id))
-		
-
-	connection.commit()
-	connection.close()
+			conn.execute("UPDATE user_meme_interaction SET score=%s WHERE user_id=%s and meme_id=%s;",(score,user_id,meme_id))
